12b6e5c6c37063735b7878dff32d76a5e3cce06d
Alejandra Higuera <al4433205@gmail.com>
2025-12-03 17:29:49 -0700

    agregue los codigos de profe para carpeta servidorMulti

--- src/ServidorMulti/UnCliente.java
+++ src/ServidorMulti/UnCliente.java
@@ -1,4 +1,39 @@
 package ServidorMulti;
+import java.io.*;
+import java.net.Socket;
 
-public class UnCliente {
+public class UnCliente implements Runnable {
+
+    final DataOutputStream salida;
+
+    final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+    final DataInputStream entrada;
+
+    UnCliente(Socket s) throws IOException {
+        salida = new DataOutputStream(s.getOutputStream());
+        entrada = new DataInputStream(s.getInputStream());
+    }
+
+    @Override
+    public void run() {
+        String mensaje;
+        while ( true ){
+            try {
+                mensaje = entrada.readUTF();
+                if (mensaje.startsWith("@")){
+                    String[]partes= mensaje.split("");
+                    String aQuien = partes[0].substring(1);
+                    UnCliente cliente = ServidorMulti.clientes.get(aQuien);
+                    cliente.salida.writeUTF(mensaje);
+
+                }else {
+                    for ( UnCliente cliente : ServidorMulti.clientes.values()){
+                        cliente.salida.writeUTF(mensaje);
+                    }  }
+            } catch (IOException ex) {
+
+            }
+        }
+    }
 }
